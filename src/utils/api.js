4f291d7f5a040b707d8d3f5e6b0b84a1463e869e
MafaldaNogueira <mafaldanogueiraapm@hotmail.com>
2020-12-03 15:07:35 +0000

    apijs

---
+++ src/utils/api.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+class BooksService {
+  constructor() {
+    let service = axios.create({
+      baseURL: `${process.env.REACT_APP_BOOKS_API}/api`,
+    });
+    this.service = service;
+  }
+
+  getBookByIsbn(isbn) {
+    return this.service.get(`/books/${isbn}`);
+  }
+
+  addBook(book) {
+    return this.service.post("/books", book);
+  }
+
+  deleteBook(isbn) {
+    return this.service.delete(`/books/${isbn}`);
+  }
+
+  editBook(updateBook) {
+    return this.service.put(`/books/${book.isbn}`, updatedBook);
+  }
+}
+
+export default BooksService;
