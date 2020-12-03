1ad1e1f36d832e53566e4540e2798dbc3d289966
Diogo Pascoal <dmtpascoal@gmail.com>
2020-12-03 17:37:24 +0000

    não sei porquê tive de aceitar

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ class App extends React.Component {
   render () {
     return (
     <div className="App">
-<<<<<<< HEAD
-      <Navbar/>
-      
-
-=======
       
     
->>>>>>> 7e5ff5d197d07b1088679ea3011288fb3d40c86e
     </div>
     )
   }
