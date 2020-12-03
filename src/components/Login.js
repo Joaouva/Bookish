c616cf17e2aa9f29dc4e09c1966ed5670e1547bf
Joao Uva <joaouva@gmail.com>
2020-12-03 16:58:19 +0000

    criei component navbar

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,3 +3,60 @@ import AuthService from '../utils/auth';
 import { Link, withRouter } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
+class Login extends React.Component {
+    state = {
+        username: '',
+        password: '',
+    }
+
+    handleChange = (event) => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+        const { username, password } = this.state;
+        const authService = new AuthService();
+        authService.login(username, password)
+            .then((response) => {
+                this.props.setCurrentUser(response.data);
+                this.props.history.push('/');
+                toast.success("Login Successfully!");
+            })
+            .catch(() => {
+            toast.error('Invalid Login')
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>Username:</label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    <button>Login</button>
+                </form>
+                <p>
+                    Don't have account?
+                    <Link to={"/signup"}> Signup</Link>
+                </p>
+            </div>
+        );
+    }
+}
+
+
+export default withRouter(Login); 
---
+++ src/components/Navbar.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import AuthService from '../utils/auth';
+
+class Navbar extends React.Component {
+
+    logoutUser = () => {
+        const authService = new AuthService();
+        authService.logout()
+            .then(() => {
+                this.props.setCurrentUser(null);
+                localStorage.removeItem('loggedInUser');
+            })
+    }
+
+    render() {
+       
+        if (this.props.loggedInUser) {
+            return (
+                <div>
+                    
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink exact to="/">
+                                    Home
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink exact to="/">
+                                    Profile
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink exact to="/">
+                                     Library
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                    
+                </div>
+            );
+        }
+        
+        else {
+            return (
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <NavLink exactto="/login">
+                                    Login
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink exact to="/signup">
+                                    Signup
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink exact to="/login-google">
+                                    Login with Google
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            );
+        }
+    }
+
+
+}
+
+export default Navbar;
