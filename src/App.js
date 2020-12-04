818154b4ac86e589ef8ad490429bf24da66eded6
Diogo Pascoal <dmtpascoal@gmail.com>
2020-12-04 10:48:38 +0000

    needed to commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import React from 'react';
+import Navbar from './components/Navbar';
 
 
 class App extends React.Component {
+  state = {
+    loggedInUser: null
+  }
   render () {
     return (
-    <div className="App">
-      
-    
-    </div>
-    )
+      <div className="App">
+        <Navbar
+          loggedInUser={this.state.loggedInUser}
+          setCurrentUser={this.setCurrentUser}
+        />
+      </div>
+    );
   }
 }
 
