6c21e4c2ec9a135afac48b5d45f458cbc53e69b1
MafaldaNogueira <mafaldanogueiraapm@hotmail.com>
2020-12-04 10:57:59 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -28,7 +12,6 @@ ReactDOM.render(
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById("root")
->>>>>>> 39fa2c95e6cbf1b40e872b37555c64a2fd643379
 );
 
 // If you want to start measuring performance in your app, pass a function
