3c4fb441417aa89e284403b2c2dc84e7f7865d22
MafaldaNogueira <mafaldanogueiraapm@hotmail.com>
2020-12-04 10:45:58 +0000

    Merge branch 'main' of https://github.com/Joaouva/bookini-client into main

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import './App.css';
-import React from 'react';
-
+import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import { Router } from "react-router-dom";
+import BooksService from "./utils/api";
 
 class App extends React.Component {
-  render () {
-    return (
-    <div className="App">
-<<<<<<< HEAD
-      <Navbar/>
-      
+  state = {
+    loggedInUser: null,
+  };
 
-=======
-      
-    
->>>>>>> 7e5ff5d197d07b1088679ea3011288fb3d40c86e
-    </div>
-    )
+  setCurrentUser = (user) => {
+    this.setState({
+      loggedInUser: user,
+    });
+  };
+
+  componentDidMount() {
+    if (this.state.loggedInUser === null) {
+      const booksService = new BooksService();
+      booksService.loggedin().then((response) => {
+        if (response.data._id) {
+          this.setCurrentUser(response.data);
+          localStorage.setItem("loggedInUser", response.data._id);
+        } else {
+          localStorage.removeItem("loggedInUser");
+        }
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar
+          loggedInUser={this.state.loggedInUser}
+          setCurrentUser={this.setCurrentUser}
+        />
+      </div>
+    );
   }
 }
 
