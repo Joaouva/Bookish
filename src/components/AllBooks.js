232c75d75ccf8421b3977a79cd2bf9e2613ca05b
Diogo Pascoal <dmtpascoal@gmail.com>
2020-12-07 12:21:25 +0000

    commit

--- src/components/AllBooks.js
+++ src/components/AllBooks.js
@@ -2,17 +2,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import BooksFromBd from "../utils/bd";
+import BooksService from "../utils/api"
 
 class GetBooksFromBd extends React.Component {
   state = {
     books: [],
+    title: '',
+    author: '',
+    image: '',
+    price: ''
   };
 
   componentDidMount() {
     const booksFromdb = new BooksFromBd();
+    const booksService = new BooksService();
     booksFromdb.getAllBooks().then((response) => {
       this.setState({
         books: response.data,
+      });
+    });
+    booksService.getBookByIsbn(this.state.books.ISBN).then((response) => {
+      this.setState({
+        title: response.data.title,
+        author: response.data.author,
+        image: response.data.image,
+        price: response.data.price,
       });
     });
   }
@@ -23,7 +37,11 @@ class GetBooksFromBd extends React.Component {
         {this.state.books.map((book, index) => {
           return (
             <div key={index}>
-              <Link to={`/books/${book.ISBN}`}>{book.ISBN}</Link>
+              <Link to={`/books/${book.ISBN}`}>
+                <img src={this.state.image}></img>
+                <h1>{this.state.title}</h1>
+                <p>{book.ISBN}</p>
+              </Link>
             </div>
           );
         })}
