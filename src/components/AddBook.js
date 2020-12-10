0206f0abccacc9aba9487d08a03b2d91a71e45c7
Joao Uva <joaouva@gmail.com>
2020-12-10 11:24:27 +0000

    last books searched

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -3,44 +3,83 @@ import BooksService from "../utils/api";
 import { withRouter, NavLink } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import BooksFromBd from "../utils/bd";
+import Card from "react-bootstrap/Card";
+import CardDeck from "react-bootstrap/CardDeck";
+import { Container, Row, Col, CardGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class AddBook extends React.Component {
-  state = {
+	state = {
     isbn: "",
-  };
+    books: []
+	};
 
-  handleChange = (event) => {
-    let { name, value } = event.target;
+	componentDidMount() {
+		const booksFromdb = new BooksFromBd();
+		booksFromdb.getAllBooks().then((response) => {
+			console.log(response);
+			this.setState({
+				books: response.data,
+			});
+		});
+	}
 
-    this.setState({
-      [name]: value,
-    });
-  };
+	handleChange = (event) => {
+		let { name, value } = event.target;
 
-  handleFormSubmit = (event) => {
-    event.preventDefault();
-    const booksService = new BooksService();
-    booksService.getBookByIsbn(this.state.isbn).then(() => {
-      this.props.history.push(`/bookfound/${this.state.isbn}`);
-    });
-  };
+		this.setState({
+			[name]: value,
+		});
+	};
 
-  render() {
-    return (
-      <div>
-        <h1> Add a new book: </h1>
-        <form onSubmit={this.handleFormSubmit}>
-          <input
-            type="text"
-            name="isbn"
-            onChange={this.handleChange}
-            value={this.state.isbn}
-          />
-          <button> Find Book </button>
-        </form>
-      </div>
-    );
-  }
+	handleFormSubmit = (event) => {
+		event.preventDefault();
+		const booksService = new BooksService();
+		booksService.getBookByIsbn(this.state.isbn).then(() => {
+			this.props.history.push(`/bookfound/${this.state.isbn}`);
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<h1> Add a new book: </h1>
+				<form onSubmit={this.handleFormSubmit}>
+					<input
+						type="text"
+						name="isbn"
+						onChange={this.handleChange}
+						value={this.state.isbn}
+					/>
+					<button> Find Book </button>
+				</form>
+				<Container>
+					<CardGroup>
+						<div className="books-wrapper">
+							{this.state.books.slice(this.state.books.length-4,this.state.books.length).map((book, index) => {
+								return (
+									<Card className="allbooks">
+										<Link to={`/books/${book.ISBN}`}>
+											{" "}
+											<Card.Img
+												variant="top"
+												src={book.image}
+												style={{ height: "21rem" }}
+											/>
+										</Link>
+										{/* <Card.Body>
+                      <Card.Title>{book.price}</Card.Title>
+                    </Card.Body> */}
+									</Card>
+								);
+							})}
+						</div>
+					</CardGroup>
+				</Container>
+			</div>
+		);
+	}
 }
 
 export default withRouter(AddBook);
